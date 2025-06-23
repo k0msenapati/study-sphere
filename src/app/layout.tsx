642941abb105c4aec93adcd11a0fa1d95f110190
Samiksha-bajoria <samikshabajoria2006@gmail.com>
2025-06-23 16:50:27 +0530

    git commit m- (feat favicon)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <head>
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+</head>
+
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider>
           {children}
