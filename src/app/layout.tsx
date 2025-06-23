21af6adb362bc50d1fc6cbc43eebc4a623ea3caa
Samiksha-bajoria <samikshabajoria2006@gmail.com>
2025-06-23 19:33:15 +0530

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
 import "./globals.css"
+import "./favicon.ico"
 import ScrollToTopButton from "@/components/ScrollToTopButton"
 import { ThemeProvider } from "@/components/theme-provider"
 
